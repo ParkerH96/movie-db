3eeddcccc137f46995a85032468b9d3319875a9a
ParkerH96 <parker.a.householder@gmail.com>
2017-03-22 16:45:59 -0400

    Update movie-db.sql

--- movie-db.sql
+++ movie-db.sql
@@ -3,13 +3,12 @@ CREATE DATABASE Databased_movie;
 
 USE Databased_movie;
 
-/* --------------Main Tables-------------- */
 DROP TABLE IF EXISTS MOVIE;
 CREATE TABLE MOVIE
 (
   movie_id int NOT NULL AUTO_INCREMENT,
   title varchar(64) NOT NULL,
-  release_date varchar(64) NOT NULL,
+  release_date date NOT NULL,
   summary varchar(1024),
   language varchar(64) NOT NULL,
   duration int NOT NULL,
@@ -24,7 +23,7 @@ CREATE TABLE USER
   first_name varchar(64) NOT NULL,
   middle_name varchar(64),
   last_name varchar(64) NOT NULL,
-  dob varchar(64) NOT NULL,
+  dob date NOT NULL,
   gender varchar(64),
   PRIMARY KEY (user_id)
 );
@@ -60,7 +59,7 @@ CREATE TABLE MEMBER
   first_name varchar(64) NOT NULL,
   middle_name varchar(64),
   last_name varchar(64) NOT NULL,
-  dob varchar(64) NOT NULL,
+  dob date NOT NULL,
   gender varchar(64),
   PRIMARY KEY (mem_id)
 );
@@ -83,7 +82,6 @@ CREATE TABLE LOGIN
   FOREIGN KEY (user_id) REFERENCES USER(user_id)
 );
 
-/* --------------Relationship Tables-------------- */
 DROP TABLE IF EXISTS has_members;
 CREATE TABLE has_members
 (
@@ -133,34 +131,65 @@ CREATE TABLE has_tags
   FOREIGN KEY (movie_id) REFERENCES MOVIE(movie_id)
 );
 
-/* --------------Example Insertions-------------- */
 INSERT INTO MOVIE(title, release_date, summary, language, duration) VALUES
-('Neighbors', '05/09/2014', 'Neighbors is a 2014 American comedy film directed by Nicholas Stoller', 'English', 97),
-('Neighbors 2', '05/20/2016', 'Now that Mac and Kelly Radner have a second baby on the way, they are ready to make the final move into adulthood: the suburbs.', 'English', 92),
-('The Notebook', '06/25/2004', 'An epic love story centered around an older man who reads aloud to an older, invalid woman whom he regularly visits.', 'English', 124),
-('The Avengers', '05/04/2012', 'Marvels The Avengers, or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name, produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures.', 'English', 143),
-('The Ring', '10/18/2002', 'It sounded like just another urban legend--videotape filled with nightmarish images, leading to a phone call foretelling the viewers death in exactly seven days.', 'English', 145),
-('American Psycho', '04/14/2000', 'Patrick Bateman is young, white, beautiful, ivy leagued, and indistinguishable from his Wall Street colleagues. Shielded by conformity, privilege, and wealth, Bateman is also the ultimate serial killer, roaming freely and fearlessly.', 'English', 104),
-('The Grudge', '10/22/2004', 'The Grudge is a 2004 American supernatural horror film and a remake of the Japanese film, Ju-on: The Grudge.', 'English', 96),
-('The Expendables', '08/13/2010', 'The Expendables is a 2010 American ensemble action film written by David Callaham and Sylvester Stallone, and directed by Stallone, who also starred in the lead role.', 'English', 113),
-('Deadpool', '02/12/2016', 'Based upon Marvel Comics most unconventional anti-hero, DEADPOOL tells the origin story of former Special Forces operative turned mercenary Wade Wilson, who after being subjected to a rogue experiment that leaves him with accelerated healing powers, adopts the alter ego Deadpool.', 'English', 108),
-('Thor', '05/05/2011', 'At the center of the story is The Mighty Thor, a powerful but arrogant warrior whose reckless actions reignite an ancient war.', 'English', 115);
+('Neighbors', '2014-05-09', 'Neighbors is a 2014 American comedy film directed by Nicholas Stoller', 'English', '01:37:00'),
+('Neighbors 2', '2016-05-20', 'Now that Mac and Kelly Radner have a second baby on the way, they are ready to make the final move into adulthood: the suburbs.', 'English', '01:32:00'),
+('The Notebook', '2004-06-25', 'An epic love story centered around an older man who reads aloud to an older, invalid woman whom he regularly visits.', 'English', '02:04:00'),
+('The Avengers', '2012-05-04', 'Marvels The Avengers, or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name, produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures.', 'English', '02:23:00'),
+('The Ring', '2002-10-18', 'It sounded like just another urban legend--videotape filled with nightmarish images, leading to a phone call foretelling the viewers death in exactly seven days.', 'English', '02:25:00'),
+('American Psycho', '2000-04-14', 'Patrick Bateman is young, white, beautiful, ivy leagued, and indistinguishable from his Wall Street colleagues. Shielded by conformity, privilege, and wealth, Bateman is also the ultimate serial killer, roaming freely and fearlessly.', 'English', '01:44:00'),
+('The Grudge', '2004-10-22', 'The Grudge is a 2004 American supernatural horror film and a remake of the Japanese film, Ju-on: The Grudge.', 'English', '01:36:00'),
+('The Expendables', '2010-08-13', 'The Expendables is a 2010 American ensemble action film written by David Callaham and Sylvester Stallone, and directed by Stallone, who also starred in the lead role.', 'English', '01:53:00'),
+('Deadpool', '2016-02-12', 'Based upon Marvel Comics most unconventional anti-hero, DEADPOOL tells the origin story of former Special Forces operative turned mercenary Wade Wilson, who after being subjected to a rogue experiment that leaves him with accelerated healing powers, adopts the alter ego Deadpool.', 'English', '01:48:00'),
+('Thor', '2011-05-05', 'At the center of the story is The Mighty Thor, a powerful but arrogant warrior whose reckless actions reignite an ancient war.', 'English', '01:45:00');
 
 INSERT INTO GENRE(genre) VALUES('Comedy'),('Romance'),('Fantasy'),('Science Fiction film'),('Drama film'),('Thriller'),('Mystery'),('Horror'),('Slasher'),('Action');
 
 INSERT INTO USER(admin_tag, first_name, middle_name, last_name, dob, gender) VALUES
-(1, 'Parker', 'Alexander', 'Householder', '05/01/1996', 'Male'),
-(1, 'Evan', 'Phillip', 'Heaton', '02/02/1995', 'Male'),
-(1, 'David', 'Clark', 'Cottrell', '04/03/1995', 'Male'),
-(1, 'John', 'Mathew', 'Dingess', '03/24/1996', 'Male'),
-(0, 'Morgan', 'Shay', 'Lewis', '10/10/1995', 'Female'),
-(0, 'Sarah', 'Elizabeth', 'Foster', '04/08/1993', 'Female'),
-(0, 'Ashley', 'Anna', 'Householder', '09/17/1992', 'Female'),
-(0, 'James', 'Bryan', 'Householder', '09/17/1963', 'Male'),
-(0, 'Monica', 'Ott', 'Householder', '03/14/1967', 'Female'),
-(0, 'Donald', 'John', 'Trump', '05/14/1946', 'Male');
+(1, 'Parker', 'Alexander', 'Householder', '1996-05-01', 'Male'),
+(1, 'Evan', 'Phillip', 'Heaton', '1995-02-02', 'Male'),
+(1, 'David', 'Clark', 'Cottrell', '1995-04-03', 'Male'),
+(1, 'John', 'Mathew', 'Dingess', '1997-01-24', 'Male'),
+(0, 'Morgan', 'Shay', 'Lewis', '1995-10-10', 'Female'),
+(0, 'Sarah', 'Elizabeth', 'Foster', '1993-04-08', 'Female'),
+(0, 'Ashley', 'Anna', 'Householder', '1992-09-17', 'Female'),
+(0, 'James', 'Bryan', 'Householder', '1963-09-17', 'Male'),
+(0, 'Monica', 'Ott', 'Householder', '1967-03-14', 'Female'),
+(0, 'Donald', 'John', 'Trump', '1946-05-14', 'Male');
 
 INSERT INTO TAGS(tag) VALUES('funny'), ('scary'), ('suspenseful'), ('silly'), ('romantic'), ('hardcore'), ('superhero'), ('marvel'), ('gory'), ('adult');
+
+INSERT INTO CREW(name) VALUES ('Deadpool-crew'), ('Thor-crew'), ('Grudge-crew'), ('Avengers-crew'), ('Neighbors-crew'), ('Expendables-crew'), ('Ring-crew'), ('Notebook-crew'), ('Psycho-crew'), ('crew10');
+
+INSERT INTO MEMBER(first_name, middle_name, last_name, dob, gender) VALUES
+('Ryan', 'Rodney', 'Reynolds', '1976-10-23', 'Male'),
+('Chris', NULL, 'Hemsworth', '1983-08-11', 'Male'),
+('Takashi', NULL, 'Shimizu', '1972-07-27', 'Male'),
+('Lisa', NULL, 'Lassek', '1970-05-21', 'Female'),
+('Zach', 'David', 'Efron', '1987-10-18', 'Male'),
+('Michael', 'Sylvester', 'Stallone', '1946-07-06', 'Male'),
+('Daveigh', 'Elizabeth', 'Chase', '1990-07-24', 'Female'),
+('Robert', NULL, 'Fraisse', '1940-05-05', 'Male'),
+('Rachel', 'Anne', 'McAdams', '1978-11-17', 'Female'),
+('Christian', 'Charles', 'Bale', '1974-01-30', 'Male');
+
+INSERT INTO ROLE(role) VALUES ('Director'), ('Producer'), ('Screenwriter'), ('Actor'), ('Editor'), ('Production Designer'), ('Art Director'), ('Extra'), ('Cinematographer'), ('Tech');
+
+INSERT INTO LOGIN VALUES
+('paho224', 'householder123', 1),
+('ephe225', 'heaton123', 2),
+('dcco226', 'cottrell123', 3),
+('jmdi227', 'dingess123', 4),
+('msle228', 'lewis123', 5),
+('sefo229', 'foster123', 6),
+('aaho230', 'householder123', 7),
+('jbho231', 'householder123', 8),
+('moho232', 'householder123', 9),
+('djtr233', 'trump123', 10);
+
+INSERT INTO has_members(crew_id, mem_id, role_id) VALUES (1, 1, 4), (2, 2, 4), (3, 3, 1), (4, 4, 5), (5, 5, 4), (6, 6, 3), (7, 7, 4), (8, 8, 9), (8, 9, 4), (9, 10, 4);
+
+INSERT INTO has_crew(movie_id, crew_id) VALUES (1, 5), (2, 5), (3, 8), (4, 4), (5, 7), (6, 9), (7, 3), (8, 6), (9, 1), (10, 2);
 
 INSERT INTO is_genres(movie_id, genre_id) VALUES (1, 1), (2, 1), (3, 2), (3, 5), (4, 3), (4, 4), (5, 6), (5, 7), (6, 5), (6, 9), (7, 6), (7, 7), (8, 10), (8, 6), (9, 10), (9, 4), (10, 3), (10, 4);
 
@@ -177,3 +206,4 @@ INSERT INTO user_actions(user_id, movie_id, rating, review) VALUES
 (8, 8, 10, 'I absolutely love sylvestor salone, and his role in this movie was outstanding!'),
 (9, 9, 10, 'This movie has to be the greatest marvel movie of all time! A must see!'),
 (10, 10, 7, 'It was good but not that good');
+
