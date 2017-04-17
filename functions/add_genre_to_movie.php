6bce7546eef4b56cfc59e24ddc989c53cd8150b7
ParkerH96 <parker.a.householder@gmail.com>
2017-04-17 01:28:53 -0400

    Fixed being able to add 2 of the same genre to a movie

--- functions/add_genre_to_movie.php
+++ functions/add_genre_to_movie.php
@@ -5,7 +5,10 @@ if(!empty($_POST['genre_select']) && !empty($_POST['movie_id'])){
   $genre_id = $mysqli->escape_string($_POST['genre_select']);
   $movie_id = $mysqli->escape_string($_POST['movie_id']);
 
-  $insertion_query = $mysqli->query("INSERT INTO is_genres VALUES ($movie_id, $genre_id)");
+  $movie_query = $mysqli->query("SELECT * FROM is_genres WHERE movie_id=$movie_id AND genre_id=$genre_id");
 
+  if($movie_query->num_rows != 1){
+    $insertion_query = $mysqli->query("INSERT INTO is_genres VALUES ($movie_id, $genre_id)");
+  }
 }
 ?>
